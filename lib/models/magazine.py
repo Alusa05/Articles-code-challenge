83bd91cbcbdb9d49975bc2f15b1e4ff01892436f
Lisa Alusa <sayilisaalusa@gmail.com>
2025-05-26 11:33:34 +0300

    completed fifth file

--- lib/models/magazine.py
+++ lib/models/magazine.py
@@ -6,4 +6,11 @@ class Magazine:
         self.title = title
         self.category = category
 
-        @class
+        @classmethod
+        def find_by_author_id(cls, author_id):
+            rows = cursor.execute("""
+                                  SELECT DISTINCT m.* FROM magazines m
+                                  JOIN articles a ON m.id = a.magazine_id
+                                  where a.author_id = ?
+                                  """,(author_id)).fetchall()
+            return [cls(*row) for row in rows]
