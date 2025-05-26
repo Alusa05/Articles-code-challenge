0fc15ec0a755253aa55e7c7512e102f507813ee6
Lisa Alusa <sayilisaalusa@gmail.com>
2025-05-26 14:58:44 +0300

    edited files

--- lib/db/connection.py
+++ lib/db/connection.py
@@ -2,5 +2,5 @@ import sqlite3
 
 def get_connection():
     conn = sqlite3.connect('articles.db')
-    conn.row_factory = sqlite3.Row  # Enable named access to columns
+    conn.row_factory = sqlite3.Row
     return conn
